eaa5a95d9cefbe6b811346ab3f5892cce0d314ad
DLGuzBuho <109259673+DanielGuzmanV@users.noreply.github.com>
2024-05-11 22:34:14 -0400

    Metodos basicos

---
+++ Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace temaMatrices
+{
+    public partial class Form1 : Form
+    {
+        classMatriz objM1, objM2, objM3, objM4;
+
+        private void descargarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            textBox5.Text = objM1.getDate();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            objM1 = new classMatriz();
+            objM2 = new classMatriz();
+            objM3 = new classMatriz();
+            objM4 = new classMatriz();
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void cargarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            objM1.setDateRandom(int.Parse(textBox1.Text), int.Parse(textBox2.Text), int.Parse(textBox3.Text), int.Parse(textBox4.Text));
+        }
+    }
+}
---
+++ classMatriz.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace temaMatrices
+{
+    class classMatriz
+    {
+        // Atributor *******************************
+        const int maxFila = 50;
+        const int maxColum = 50;
+        private int[,] matriz;
+        private int fila, colum;
+
+        // constructor ****************************
+        public classMatriz()
+        {
+            matriz = new int[maxFila, maxColum];
+            fila = 0; colum = 0;
+        }
+ 
+        // los metodos de la clase Matriz *****************************
+        // Cargar randomicamente la matriz
+        public void setDateRandom(int numFila, int numColum, int min, int max)
+        {
+            Random rand = new Random();
+            this.fila = numFila; this.colum = numColum;
+            for(int fil = 1; fil <= this.fila; fil++)
+            {
+                for(int col = 1; col <= this.colum; col++)
+                {
+                    matriz[fil, col] = rand.Next(min, max + 1);
+                }
+            }
+        }
+        // Descargar la matriz 
+        public string getDate()
+        {
+            string space = "";
+            for(int fil = 1; fil <= this.fila; fil++)
+            {
+                for(int col = 1; col <= this.colum; col++)
+                {
+                    space = space + matriz[fil, col] + "\t";
+                }
+                space = space + "\x000d" + "\x000a";
+            }
+            return space;
+        }
+    }
+}
